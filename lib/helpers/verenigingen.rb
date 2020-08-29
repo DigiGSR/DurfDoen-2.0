cb285c2f1eced18c60626d670b00513e3ea37b5b
mcbloch <maxime.bloch@ugent.be>
2020-08-29 16:01:18 +0200

    Fix image function

--- lib/helpers/verenigingen.rb
+++ lib/helpers/verenigingen.rb
@@ -51,17 +51,15 @@ module VerenigingenHelper
     else
       item[:image]
     end
-    def abbreviation(item)
-        item.identifier.without_ext.split('/').last
-    end
-    def verenigingen_random(amount)
-        @items.find_all("**/verenigingen/*").map{|x| {
-            "titel" => x[:titel],
-            "naam" => x[:naam],
-            "konvent" => x[:konvent],
-            "themas" => x[:themas]
-        }}.to_a.shuffle[0..amount]
-    end
+  end
+
+  def verenigingen_random(amount)
+    @items.find_all("**/verenigingen/*").map { |x| {
+        "titel" => x[:titel],
+        "naam" => x[:naam],
+        "konvent" => x[:konvent],
+        "themas" => x[:themas]
+    } }.to_a.shuffle[0..amount]
   end
 
   def image_tag(item)
